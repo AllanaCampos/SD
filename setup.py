55dc4a56ee1c1b2b4c606c63349c1f692ce3c1a9
Allana Campos <allanacampos50@gmail.com>
2021-08-31 07:52:20 -0300

    teste

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ setup(
     name='pratica-1',
     description='Pratica 1 de SD, 2021.2',
     author='Allana Campos',
-    install_requires=['fastapi', 'uvicorn'],
+    install_requires=[],
     packages=['app'],
-    entry_points={
-        'console_scripts': [
-            'pratica-1=app.app:main',
-        ]
-    }
+
 )
