b408ae7f527402105f9befe2ec7f776968217429
Allana Campos <allanacampos50@gmail.com>
2021-11-30 12:02:18 -0300

    eleicao

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ def main():
 
     loop.create_task(server.serve())
     loop.create_task(coordenador_inicial())
-    loop.create_task(verify_event())
+    #loop.create_task(verify_event())
     loop.run_forever()
 
 
